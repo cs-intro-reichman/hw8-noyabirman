44eacb5be34f9f37f46fd91a72dcf56f74924383
Noya Birman <noya.birman@post.runi.ac.il>
2025-01-08 18:48:41 +0200

    try

--- Network.java
+++ Network.java
@@ -111,9 +111,9 @@ public class Network {
     // Returns a textual description of all the users in this network, and who they follow.
     public String toString() {
         String s= "Network:";
-       for (int i =0 ; i<userCount ; i++){
-        s =s+  "\n"+  users[i].getName()+ " -> ";
-       }
-       return s;
+        for (int i =0 ; i<userCount ; i++){
+            s =s+  "\n"+  users[i].getName()+ " -> ";
+        }
+        return s;
     }
 }
